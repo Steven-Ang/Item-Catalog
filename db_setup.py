5875a0e40389dcd06401c893529492f298a26da4
Steven Ang <spikegreen2@gmail.com>
2017-09-14 20:11:41 +0800

    Add JSON serialization to the Album table

--- db_setup.py
+++ db_setup.py
@@ -19,6 +19,18 @@ class Album(Base):
     number_of_track = Column(String(2), nullable=False)
     cover = Column(String(300), nullable=False)
 
+    # Returns object data in easily serializeable format
+    @property
+    def serialize(self):
+        return {
+            "title": self.title,
+            "artist": self.artist,
+            "genre": self.genre,
+            "release_date": self.release_date,
+            "number_of_track": self.number_of_track,
+            "cover": self.cover
+        }
+
 
 engine = create_engine('sqlite:///music.db')
 
