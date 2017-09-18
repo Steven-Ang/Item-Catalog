9460e5e34a70d7c80a4e18f805656b7a7559b2a6
Steven Ang <spikegreen2@gmail.com>
2017-09-18 19:22:30 +0800

    Move route: /login, /gconnect, /gdisconnect to the bottom of the file

--- application.py
+++ application.py
@@ -33,6 +33,97 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+# Show all of the existing albums
+# from the database on the page "/albums/"
+# and the root of the web application.
+@app.route("/albums/")
+@app.route("/")
+def showAlbums():
+    albums = session.query(Album).all()
+    return render_template("catalog.html", albums=albums)
+
+
+# Show individual album
+@app.route("/albums/<int:album_id>/")
+def showAlbum(album_id):
+    album = session.query(Album).filter_by(id=album_id).one()
+    return render_template("album.html", album=album)
+
+
+# Authenticated users have the ability to add new album to the database
+# by going to the page "/albums/new".
+# Unauthenticated users are not permitted to access this page.
+@app.route("/albums/new/", methods=["GET", "POST"])
+def createAlbum():
+    if request.method == "POST":
+        newAlbum = Album(
+            title=request.form['title'],
+            artist=request.form["artist"],
+            genre=request.form["genre"],
+            release_date=request.form["release_date"],
+            number_of_track=request.form["number_of_track"],
+            cover=request.form["cover"])
+        session.add(newAlbum)
+        session.commit()
+        flash("Success! A new album has been added to the database!")
+        return redirect(url_for('showAlbums'))
+    else:
+        return render_template("createAlbum.html")
+
+
+# Edit album from the database
+@app.route("/albums/edit/<int:album_id>/", methods=["GET", "POST"])
+def editAlbum(album_id):
+    albumToEdit = session.query(Album).filter_by(id=album_id).one()
+    if request.method == "POST":
+        if request.form["title"]:
+            albumToEdit.title = request.form["title"]
+            albumToEdit.artist = request.form["artist"]
+            albumToEdit.genre = request.form["genre"]
+            albumToEdit.release_date = request.form["release_date"]
+            albumToEdit.number_of_track = request.form["number_of_track"]
+            albumToEdit.cover = request.form["cover"]
+            session.add(albumToEdit)
+            session.commit()
+            flash("Success! The album has been edited!")
+        return redirect(url_for("showAlbums"))
+    else:
+        return render_template(
+            "editAlbum.html",
+            album=albumToEdit,
+            album_id=album_id)
+
+
+# Delete album from the database
+@app.route("/albums/delete/<int:album_id>/", methods=["GET", "POST"])
+def deleteAlbum(album_id):
+    albumToDelete = session.query(Album).filter_by(id=album_id).one()
+    if request.method == "POST":
+        session.delete(albumToDelete)
+        session.commit()
+        flash("Success! One album has been removed from the database!")
+        return redirect(url_for("showAlbums"))
+    else:
+        return render_template(
+            "deleteAlbum.html",
+            album=albumToDelete,
+            album_id=album_id)
+
+
+# Endpoint for the all of the album from the catalog
+@app.route("/albums/JSON/")
+def showAlbumsJson():
+    albumsJSON = session.query(Album).all()
+    return jsonify(Album=[i.serialize for i in albumsJSON])
+
+
+# Endpoint for individual album
+@app.route("/albums/<int:album_id>/JSON/")
+def showAlbumJson(album_id):
+    albumJSON = session.query(Album).filter_by(id=album_id).one()
+    return jsonify(Album=albumJSON.serialize)
+
+
 # Create anti-forgery state token
 @app.route('/login')
 def showLogin():
@@ -158,97 +249,6 @@ def gdisconnect():
         response = make_response(json.dumps('Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
         return response
-
-
-# Show all of the existing albums
-# from the database on the page "/albums/"
-# and the root of the web application.
-@app.route("/albums/")
-@app.route("/")
-def showAlbums():
-    albums = session.query(Album).all()
-    return render_template("catalog.html", albums=albums)
-
-
-# Show individual album
-@app.route("/albums/<int:album_id>/")
-def showAlbum(album_id):
-    album = session.query(Album).filter_by(id=album_id).one()
-    return render_template("album.html", album=album)
-
-
-# Authenticated users have the ability to add new album to the database
-# by going to the page "/albums/new".
-# Unauthenticated users are not permitted to access this page.
-@app.route("/albums/new/", methods=["GET", "POST"])
-def createAlbum():
-    if request.method == "POST":
-        newAlbum = Album(
-            title=request.form['title'],
-            artist=request.form["artist"],
-            genre=request.form["genre"],
-            release_date=request.form["release_date"],
-            number_of_track=request.form["number_of_track"],
-            cover=request.form["cover"])
-        session.add(newAlbum)
-        session.commit()
-        flash("Success! A new album has been added to the database!")
-        return redirect(url_for('showAlbums'))
-    else:
-        return render_template("createAlbum.html")
-
-
-# Edit album from the database
-@app.route("/albums/edit/<int:album_id>/", methods=["GET", "POST"])
-def editAlbum(album_id):
-    albumToEdit = session.query(Album).filter_by(id=album_id).one()
-    if request.method == "POST":
-        if request.form["title"]:
-            albumToEdit.title = request.form["title"]
-            albumToEdit.artist = request.form["artist"]
-            albumToEdit.genre = request.form["genre"]
-            albumToEdit.release_date = request.form["release_date"]
-            albumToEdit.number_of_track = request.form["number_of_track"]
-            albumToEdit.cover = request.form["cover"]
-            session.add(albumToEdit)
-            session.commit()
-            flash("Success! The album has been edited!")
-        return redirect(url_for("showAlbums"))
-    else:
-        return render_template(
-            "editAlbum.html",
-            album=albumToEdit,
-            album_id=album_id)
-
-
-# Delete album from the database
-@app.route("/albums/delete/<int:album_id>/", methods=["GET", "POST"])
-def deleteAlbum(album_id):
-    albumToDelete = session.query(Album).filter_by(id=album_id).one()
-    if request.method == "POST":
-        session.delete(albumToDelete)
-        session.commit()
-        flash("Success! One album has been removed from the database!")
-        return redirect(url_for("showAlbums"))
-    else:
-        return render_template(
-            "deleteAlbum.html",
-            album=albumToDelete,
-            album_id=album_id)
-
-
-# Endpoint for the all of the album from the catalog
-@app.route("/albums/JSON/")
-def showAlbumsJson():
-    albumsJSON = session.query(Album).all()
-    return jsonify(Album=[i.serialize for i in albumsJSON])
-
-
-# Endpoint for individual album
-@app.route("/albums/<int:album_id>/JSON/")
-def showAlbumJson(album_id):
-    albumJSON = session.query(Album).filter_by(id=album_id).one()
-    return jsonify(Album=albumJSON.serialize)
 
 
 if __name__ == "__main__":
